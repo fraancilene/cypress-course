a050809ea6927efa92772ce1f5126221fa44055d
fraanpsilva <francilenesilva.pt@gmail.com>
2023-06-15 11:31:16 +0100

    config projec and run first testt

---
+++ cypress/integration/CAC-TAT.spec.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+
+describe('Central de Atendimento ao Cliente TAT', function(){
+    it('verifica o titulo da aplicação', function(){
+        cy.visit('/src/index.html');
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT');
+    });
+});
